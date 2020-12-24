c6b95a586f3472bcb2621742544638566a9c23f5
memo40 <memocann4040@gmail.com>
2020-12-24 23:25:47 +0100

    addet welcome js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const click = document.querySelector("button");
 const div = document.querySelector("body");
 const a =document.querySelectorAll("div");
-
+const text=document.getElementById("text");
+const section =document.querySelector("section");
 
 function animat(){
     div.style.marginLeft="3000px";
@@ -12,7 +13,42 @@ function animat(){
     }
 
 
-click.addEventListener("click",animat)
+
+function appear(){
+    setTimeout(() => {
+        section.animate({
+        "opacity":"4"
+    },4000)
+    }, 3000);
+    
+    setTimeout(() => {
+        section.style.opacity="4"
+    }, 7000);
+    
+}
+
+function slide(){
+
+    setTimeout(() => {
+        text.animate({"opacity":"4"},3000)
+    }, 2000);
+    
+    setTimeout(() => {
+        text.style.opacity="4";
+        
+    }, 5000);
+}
+
+
+
+
+
+
+
+
+
+
+click.addEventListener("click",animat);
 a.addEventListener("mousemove", (event)=>{
     event.target.style.color="red";
     event.target.style.transition="2s";
