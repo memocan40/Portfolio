07825d7cf35886fd70f144e3dc0fb96dbae4b35e
memo <memocann94@gmail.com>
2022-10-11 22:26:34 +0200

    github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,7 +23,7 @@ function Footer() {
                     <ul className="footer-icons">
                         <li className="social-icons">
                             <a
-                                href="https://github.com/memocan040"
+                                href="https://github.com/memocan40"
                                 style={{ color: "white" }}
                                 target="_blank"
                                 rel="noopener noreferrer"
