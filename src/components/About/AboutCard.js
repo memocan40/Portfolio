12e5c7571c75aa7bf0a675bb7a1309b26538b34a
memo <memocann94@gmail.com>
2022-10-11 21:35:30 +0200

    aktualisieren

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,9 +9,8 @@ function AboutCard() {
                 <blockquote className="blockquote mb-0">
                     <p style={{ textAlign: "justify" }}>
                         Hi Everyone, I am <span className="purple">Mehmet Can  </span>
-                        from <span className="purple"> Bhubaneswar, India.</span>
-                        <br />I am a junior pursuing IMSC in Maths and Computing in BIT
-                        Mesra.
+                        from <span className="purple"> "TR".</span>
+                        <br />I am a junior Developer
                         <br />
                         <br />
                         Apart from coding, some other activities that I love to do!
@@ -24,7 +23,7 @@ function AboutCard() {
                             <ImPointRight /> Writting Tech Blogs
                         </li>
                         <li className="about-activity">
-                            <ImPointRight /> Travelling
+                            <ImPointRight /> Coding, Learning
                         </li>
                     </ul>
 
