4b13556fd4a2200889ecd7b0bf465d45692b6517
M40 <memocann94@gmail.com>
2023-07-26 23:45:39 +0200

    addet and cleaned projects unneccessary

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,13 +7,7 @@ import food from "../../Assets/Projects/foodnet.png";
 import todo from "../../Assets/Projects/todolist.jpg";
 import calcul from "../../Assets/Projects/calcula.jpg";
 import flappy from "../../Assets/Projects/flappy.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
-import button from "../../Assets/Projects/button.png";
-import javainteractive from "../../Assets/Projects/javaframe.png";
-import pythonpress from "../../Assets/Projects/pythonkeypress.png";
-import cflappy from "../../Assets/Projects/cflappy.png";
-import angular from "../../Assets/Projects/Angular.jpg";
-import angulardotnet from "../../Assets/Projects/angularanddotnetcore.jpg";
+import reactpage from "../../Assets/Projects/reactpage.png";
 
 function Projects() {
     return (
@@ -31,15 +25,14 @@ function Projects() {
 
                 <Col md={4} className="project-card">
                         <ProjectCard
-                            imgPath={angular}
+                            imgPath={reactpage}
                             isBlog={false}
-                            title="MyAngular"
-                            description="A Angular App in the making. .Net for Backend and MySQL Server"
-                            ghLink="https://github.com/memocan40/AngularProject"
-                            demoLink="https://memocan40.github.io/AngularProject/"
+                            title="Landing Page"
+                            description="Deployed on Netlify a Landing Page in React "
+                            ghLink="https://github.com/memocan40/React-Page"
+                            demoLink="https://master--stellular-cajeta-0cd5a7.netlify.app/"
                         />
                     </Col>
-
 
                     <Col md={4} className="project-card">
                         <ProjectCard
@@ -86,33 +79,6 @@ function Projects() {
                         />
                     </Col>
 
-                    <Col md={4} className="project-card">
-                        <ProjectCard
-                            imgPath={javainteractive}
-                            isBlog={false}
-                            title="A Interactive Frame in Java just a fun Project"
-                            description="you can download the jar file on github (download button for the jar in portfolio coming soon)"
-                            ghLink="https://github.com/memocan40/Java-Interactive"
-                        />
-                    </Col>
-                    <Col md={4} className="project-card">
-                        <ProjectCard
-                            imgPath={pythonpress}
-                            isBlog={false}
-                            title="A keypressing tool (you can choose on your own which key to be pressed permamently) for example for online games to farm automaticly the python file was converted to an exe with pytoexe download coming soon"
-                            description="you can download the exe file on github (download button for the jar in portfolio coming soon)"
-                            ghLink="https://github.com/memocan40/pythonKeypress"
-                        />
-                    </Col>
-                    <Col md={4} className="project-card">
-                        <ProjectCard
-                            imgPath={cflappy}
-                            isBlog={false}
-                            title="Windows forms game like flappybirths"
-                            description="A Windows Forms game in FLappy birds"
-                            ghLink="https://github.com/memocan40/C-Flappy"
-                        />
-                    </Col>
                     
                     <Col md={4} className="project-card">
                         <ProjectCard
